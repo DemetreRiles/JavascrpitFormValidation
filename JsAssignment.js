34dc388125de539074abc9b04628ab90c7632802
DemetreRiles <46505897+DemetreRiles@users.noreply.github.com>
2019-02-21 19:35:01 -0500

    Add files via upload

---
+++ JsAssignment.js
@@ -0,0 +1,19 @@
+function formValidate() {
+  var x = document.forms ["myForm"]["fname"].value;
+  var y = document.forms ["myForm"]["lname"].value;
+  var z = document.forms ["myForm"]["email"].value;
+
+  if (x == "") {
+    alert("First name must be filled out");
+    return false;
+  }
+  if(y == "") {
+    alert("Last name must be filled out");
+    return false;
+  }
+  if(z == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+
+}
